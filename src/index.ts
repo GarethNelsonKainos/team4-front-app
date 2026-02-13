711067296daa14af61ad8fa5ee578219ee02e696
CameronStewartKainos <cameron.stewart@kainos.com>
2026-02-13 11:39:47 +0000

    re-added line

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ nunjucksEnv.addFilter("formatDate", (dateString: string) => {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
