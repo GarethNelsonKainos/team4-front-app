aaf6b3fb8085de2e1858d989a3d1fe134ef80eed
BenKainos-43 <ben.malcomson@kainos.com>
2026-02-25 15:22:00 +0000

    Removed comments

--- tests/bdd/steps/login.steps.ts
+++ tests/bdd/steps/login.steps.ts
@@ -16,8 +16,3 @@ When('I login with email {string} and password {string}', async function(this: C
 Then('I should see the jobs listings page', async function(this: CustomWorld) {
   await expect(this.page).toHaveURL(/\/jobs/);
 });
-
-// Then('the navigation should show my email', async function(this: CustomWorld) {
-  // Add assertion for email visibility in nav
-  // Example: await expect(this.page.locator('nav')).toContainText('test@example.com');
-// });
