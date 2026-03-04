50958a06c2bd7379369e4e06f09db063e86873bb
BenKainos-43 <ben.malcomson@kainos.com>
2026-03-04 09:16:57 +0000

    Small fixes

--- my-infrastructure/environments/dev/provider.tf
+++ my-infrastructure/environments/dev/provider.tf
@@ -6,14 +6,9 @@ terraform {
     }
   }
 
-
-  # Backend block
-  backend "azurerm" {
-    resource_group_name  = "rg-tf-state-dev-ben-new"
-    storage_account_name = "tfstatebenteam4new"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
+  # Backend config supplied at init time via -backend-config flags
+  # or a backend.tfvars file (not committed to version control)
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
