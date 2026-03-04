2c51ed2e0bb9895049ad0c041a93d23c023fd988
BenKainos-43 <ben.malcomson@kainos.com>
2026-03-04 11:45:54 +0000

    removed default for environment in variables.tf

--- my-infrastructure/environments/dev/variables.tf
+++ my-infrastructure/environments/dev/variables.tf
@@ -30,7 +30,6 @@ variable "location" {
 variable "environment" {
   description = "Deployment environment (dev, test, prod)."
   type        = string
-  default     = "dev"
 
   validation {
     condition     = contains(["dev", "test", "prod"], var.environment)
